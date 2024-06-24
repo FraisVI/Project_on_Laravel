1888f01bb3b904599a026b6e3802b73c5dc801bb
FraisVI <FraisVI@yandex.ru>
2024-06-24 14:06:48 +0700

    Less 28,29 Factory for database and pagination

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -5,7 +5,10 @@
             <a href="{{ route('post.create') }}" class="btn btn-secondary mb-3">Create post</a>
         </div>
         @foreach($posts as $post)
-            <div><a href="{{ route('post.show', $post->id) }}"> {{ $post->id }}. {{ $post->title }}</a></div>
+            <div><a href="{{ route('post.show', $post->id) }}" style="text-decoration: none"> {{ $post->id }}. {{ $post->title }}</a></div>
         @endforeach
+        <div class="mt-3">
+            {{ $posts->links() }}
+        </div>
     </div>
 @endsection

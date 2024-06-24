8f78493a33daefae905de7303d94ad7f561b824c
FraisVI <FraisVI@yandex.ru>
2024-06-24 20:00:12 +0700

    Less 30 Filter

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -3,16 +3,26 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\FilterRequest;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\Tag;
 
 class IndexController extends BaseController
 {
-    public function __invoke()
+    public function __invoke(FilterRequest $request)
     {
-        $posts = Post::paginate(10);
+        $data = $request->validated();
+        $query = Post::query();
 
-        return view('post.index', compact('posts'));
+        if (isset($data['category_id'])) {
+            $query->where('category_id', $data['category_id']);
+        }
+        $posts = $query->get();
+        dd($posts);
+
+/*        $posts = Post::paginate(10);
+
+        return view('post.index', compact('posts'));*/
     }
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -32,7 +32,7 @@ class PostController extends Controller
     public function store()
     {
         $data = request()->validate([
-            'title' => 'required|string',
+            'title' => 'string',
             'content' => 'string',
             'image' => 'string',
             'category_id' => '',
